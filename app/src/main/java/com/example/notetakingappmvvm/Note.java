05cb97dcc4d2ef0be814de25d229b2c637b3f179
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2021-06-13 12:24:43 +0530

    Entity Class Added using Room Lib

---
+++ app/src/main/java/com/example/notetakingappmvvm/Note.java
@@ -0,0 +1,49 @@
+package com.example.notetakingappmvvm;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+/*
+Entities are the objects that we have to store in DB
+All the fields of this class will bw added as table in the DB
+Room lib saves us from writing boiler plate code for SQLITE
+ */
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String title;
+    private String description;
+//    @ColumnInfo(name = "priority_column") // this is how you change the name of col in table
+    private int priority;
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
