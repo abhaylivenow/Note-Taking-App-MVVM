174ee3fc2b3f864ca2ef32f57095070e5a13fa58
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2021-06-13 15:21:53 +0530

    Room callback added to the NotesDatabase class

--- app/src/main/java/com/example/notetakingappmvvm/NotesDatabase.java
+++ app/src/main/java/com/example/notetakingappmvvm/NotesDatabase.java
@@ -1,10 +1,14 @@
 package com.example.notetakingappmvvm;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
 /*
  We have to define this class as Singleton
  */
@@ -17,9 +21,34 @@ public abstract class NotesDatabase extends RoomDatabase {
     public static synchronized NotesDatabase getInstance(Context context){
         if(instance == null){
             instance = Room.databaseBuilder(context.getApplicationContext(),NotesDatabase.class
-            ,"note_database").fallbackToDestructiveMigration().build();
+            ,"note_database")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomCallBack)
+                    .build();
         }
         return instance;
     }
+    private static RoomDatabase.Callback roomCallBack = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsync(instance).execute();
+        }
+    };
+    // Fill the database with some default note when the app opens up for the first time
+    private static class PopulateDbAsync extends AsyncTask<Void,Void,Void>{
+        private NoteDao noteDao;
 
+        private PopulateDbAsync(NotesDatabase db){
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title1","Desc1",1));
+            noteDao.insert(new Note("Title2","Desc2",2));
+            noteDao.insert(new Note("Title3","Desc3",3));
+            return null;
+        }
+    }
 }
